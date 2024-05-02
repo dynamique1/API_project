b80f68599fb1d436077c28590fd78f62129caa93
dynamique1 <bee4keys@gmail.com>
2024-05-02 21:35:03 +0100

    api_scraped

--- api_scrpaing_project/api_project/api_project/spiders/quote_scraper.py
+++ api_scrpaing_project/api_project/api_project/spiders/quote_scraper.py
@@ -1,10 +1,19 @@
 import scrapy
+import json
 
 
 class QuoteScraperSpider(scrapy.Spider):
     name = "quote_scraper"
     allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ["https://quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/api/quotes?page=1"]
 
     def parse(self, response):
-        pass
+        json_response = json.loads(response.body)
+        quotes = json_response.get('quotes')
+        for quote in quotes:
+            yield{
+                'author':quote.get('author').get('name'),
+                'tags':quote.get('tags'),
+                'text':quote.get('text'),
+            }
+
